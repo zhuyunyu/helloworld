933aa2992bdd23b68054d6a525ed6ef9d4255b3d
zhuyunyu <58204477+zhuyunyu@users.noreply.github.com>
2019-12-07 16:55:00 +0800

    Update hello.c

--- hello.c
+++ hello.c
@@ -10,7 +10,7 @@ int main(void)
   if(curl){
     curl_easy_setopt(curl,CURLOPT_URL,"http://myip.ipip.net/");
     res=curl_easy_perform(curl);
-    if(res!=CURL_OK)
+    if(res!=CURLE_OK)
       fprintf(stderr,"curl_easy_perform() failed:%s\n",
             curl_easy_strerror(res));
       curl_easy_cleanup(curl);
